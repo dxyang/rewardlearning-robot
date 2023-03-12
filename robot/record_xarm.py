e35e584a0268509c2cabecc88cb4ad647f382de0
Jacob6819 <hockeyviolin@gmail.com>
2023-03-12 14:35:17 -0700

    updated

--- robot/record_xarm.py
+++ robot/record_xarm.py
@@ -34,25 +34,26 @@ from robot.data import RoboDemoDset
 from robot.utils import HZ
 from robot.xarm_env import XArmEnv
 
+
 class ArgumentParser(Tap):
     # fmt: off
-    task: str                                           # Task ID for demonstration collection
-    data_dir: Path = Path("data/demos/")                # Path to parent directory for saving demonstrations
+    task: str  # Task ID for demonstration collection
+    data_dir: Path = Path("data/demos/")  # Path to parent directory for saving demonstrations
 
     use_gripper: bool = False
 
     # Task Parameters
-    include_visual_states: bool = True                  # Whether to run playback/get visual states (only False for now)
-    max_time_per_demo: int = 21                         # Max time (in seconds) to record demo -- default = 21 seconds
+    include_visual_states: bool = True  # Whether to run playback/get visual states (only False for now)
+    max_time_per_demo: int = 21  # Max time (in seconds) to record demo -- default = 21 seconds
 
-    random_reset: bool = False                          # randomly initialize home pose with an offset
+    random_reset: bool = False  # randomly initialize home pose with an offset
 
     # Collection Parameters
-    collection_strategy: str = "kinesthetic"            # How demos are collected :: only `kinesthetic` for now!
-    controller: str = "joint"                           # Demonstration & playback uses a Joint Impedance controller...
-    resume: bool = True                                 # Resume demonstration collection (on by default)
+    collection_strategy: str = "kinesthetic"  # How demos are collected :: only `kinesthetic` for now!
+    controller: str = "joint"  # Demonstration & playback uses a Joint Impedance controller...
+    resume: bool = True  # Resume demonstration collection (on by default)
 
-    plot_trajectory: bool = False                       # generate html plot for visualizing trajectory
+    plot_trajectory: bool = False  # generate html plot for visualizing trajectory
     show_viewer: bool = False
     # fmt: on
 
@@ -69,22 +70,23 @@ def demonstrate() -> None:
         os.makedirs(demo_rgb_dir, exist_ok=args.resume)
 
     # data saving yay
-    video_recorder = VideoRecorder(save_dir = demo_rgb_dir, fps=HZ)
+    video_recorder = VideoRecorder(save_dir=demo_rgb_dir, fps=HZ)
     h5py_dset_path = args.data_dir / args.task / "demos.hdf"
     dset = RoboDemoDset(save_path=h5py_dset_path, read_only_if_exists=False)
 
     # Initialize environment in `record` mode...
     print("[*] Initializing Robot Connection...")
-    # env = XArmTaskSpaceEnv(
-    #     control_frequency_hz=HZ,
-    #     use_camera=args.include_visual_states,
-    #     use_gripper=args.use_gripper, # TODO: create some sort of button pressing mechanism to open and close the gripper,
-    #     random_reset_home_pose=args.random_reset
-    # )
-    env = XArmEnv(control_frequency_hz=HZ, control_mode='angular',
-                  use_camera=args.include_visual_states,
-                  use_gripper=args.use_gripper,
-                  random_reset_home_pose=args.random_reset)
+    env = XArmTaskSpaceEnv(
+        control_frequency_hz=HZ,
+        use_camera=args.include_visual_states,
+        use_gripper=args.use_gripper,
+        # TODO: create some sort of button pressing mechanism to open and close the gripper,
+        random_reset_home_pose=args.random_reset
+    )
+    # env = XArmEnv(control_frequency_hz=HZ, control_mode='angular',
+    #               use_camera=args.include_visual_states,
+    #               use_gripper=args.use_gripper,
+    #               random_reset_home_pose=args.random_reset)
 
     # Initializing Button Control... TODO(siddk) -- switch with ASR
     # print("[*] Connecting to Button Controller...")
@@ -108,6 +110,7 @@ def demonstrate() -> None:
 
         # Reset environment & wait on user input...
         obs = env.reset()
+        print('here')
         env.set_mode("record")
 
         print(
@@ -117,17 +120,16 @@ def demonstrate() -> None:
         )
         # Loop on valid button input...
 
-
         console = input()
         # print(console)
         a = console == 'a'
-        x = console =='x'
-        y = console =='y'
+        x = console == 'x'
+        y = console == 'y'
         while not a and not y:
             console = input()
             a = console == 'a'
-            x = console =='x'
-            y = console =='y'
+            x = console == 'x'
+            y = console == 'y'
 
         # Quit if (Y)...
         if y:
@@ -135,6 +137,7 @@ def demonstrate() -> None:
 
         # Go, go, go!
         print("\t=>> Started recording... press (X) to terminate recording!")
+
         # rgbs = []
 
         def isData():
@@ -142,35 +145,35 @@ def demonstrate() -> None:
 
         old_settings = termios.tcgetattr(sys.stdin)
         try:
-                tty.setcbreak(sys.stdin.fileno())
-                ee_poses = []
-                angle_poses = []
-                print(args.max_time_per_demo)
-                print(HZ)
-                for _ in range(int(args.max_time_per_demo * HZ) - 1):
-                    # visualize if camera
-                    if args.show_viewer:
-                        bgr = cv2.cvtColor(obs["rgb_image"], cv2.COLOR_RGB2BGR)
-                        cv2.imshow('RGB', bgr)
-                        cv2.waitKey(1)
+            tty.setcbreak(sys.stdin.fileno())
+            ee_poses = []
+            # angle_poses = []
+            # print(args.max_time_per_demo)
+            # print(HZ)
+            for _ in range(int(args.max_time_per_demo * HZ) - 1):
+                # visualize if camera
+                if args.show_viewer:
+                    bgr = cv2.cvtColor(obs["rgb_image"], cv2.COLOR_RGB2BGR)
+                    cv2.imshow('RGB', bgr)
+                    cv2.waitKey(1)
 
-                    # Get Button Input (only if True) --> handle extended button press...
+                # Get Button Input (only if True) --> handle extended button press...
 
-                    # Terminate...
-                    if isData():
-                        c = sys.stdin.read(1)
-                        print(c)
-                        if c == 'x':         # x1b is ESC
-                                print("\tHit (X) - stopping recording...")
-                                break
-                    # Otherwise no termination, keep on recording...
-                    else:
-                        obs, _, _, _ = env.step(None)
-                        ee_poses.append(obs["ee_pos"])
-                        angle_poses.append(obs['q'])
-                        # rgbs.append(obs["rgb_image"])
+                # Terminate...
+                if isData():
+                    c = sys.stdin.read(1)
+                    print(c)
+                    if c == 'x':  # x1b is ESC
+                        print("\tHit (X) - stopping recording...")
+                        break
+                # Otherwise no termination, keep on recording...
+                else:
+                    obs, _, _, _ = env.step(None)
+                    ee_poses.append(obs["ee_pos"])
+                    # angle_poses.append(obs['q'])
+                    # rgbs.append(obs["rgb_image"])
         finally:
-                termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
         # Drop into Recording Loop --> for `record` mode, we really only care about joint positions
         #   =>> TODO(siddk) - handle Gripper?
@@ -185,7 +188,7 @@ def demonstrate() -> None:
         #         cv2.waitKey(1)
 
         #     # Get Button Input (only if True) --> handle extended button press...
-            
+
         #     # console = input()
         #     # print(console)
         #     # a = console == 'a'
@@ -197,10 +200,9 @@ def demonstrate() -> None:
         #     #         global opt 
         #     #         opt = key.char
         #     #         return False
-            
+
         #     # with Listener(on_press=on_press) as listener:
         #     #     listener.join()
-
 
         #     console = input()
         #     # print(console)
@@ -232,13 +234,13 @@ def demonstrate() -> None:
             print("[*] Optional -- would you like to replay demonstration? Press (A) to playback, and (X) to continue!")
             console = input()
             a = console == 'a'
-            x = console =='x'
-            y = console =='y'
+            x = console == 'x'
+            y = console == 'y'
             while not a and not x:
                 console = input()
                 a = console == 'a'
-                x = console =='x'
-                y = console =='y'
+                x = console == 'x'
+                y = console == 'y'
 
             # Skip Playback!
             if x:
@@ -255,26 +257,27 @@ def demonstrate() -> None:
             # eef_xyzs.append(obs["ee_pose"][:3].copy())
             # jas.append(obs["q"].copy())
             rgbs.append(obs["rgb_image"].copy())
-
+            # jas.append(obs['q'])
+            eef_poses.append(obs['ee_pos'])
 
             # Block on User Ready -- Robot will move, so this is for safety...
             print("\tReady to playback! Get out of the way, and hit (A) to continue...")
             console = input()
             a = console == 'a'
-            x = console =='x'
-            y = console =='y'
+            x = console == 'x'
+            y = console == 'y'
             while not a:
                 console = input()
                 a = console == 'a'
-                x = console =='x'
-                y = console =='y'
+                x = console == 'x'
+                y = console == 'y'
 
             # Execute Trajectory
             print("\tReplaying...")
-            for idx in range(len(angle_poses)):
-                obs, _, _, _ = env.step(angle_poses[idx])
+            for idx in range(len(ee_poses)):
+                obs, _, _, _ = env.step(ee_poses[idx])
                 rgbs.append(obs["rgb_image"].copy())
-                jas.append(obs['q'])
+                # jas.append(obs['q'])
                 eef_poses.append(obs['ee_pos'])
             # Close Environment
             env.close()
@@ -288,7 +291,7 @@ def demonstrate() -> None:
             # eef_xyzs_np = np.array(eef_xyzs).T # N x 3
             plot_points_sequence(scene.figure, points=eef_xyzs_np)
             scene.plot_scene_to_html("test")
-        
+
         print(len(rgbs))
 
         # Move on?
@@ -297,13 +300,13 @@ def demonstrate() -> None:
         # Loop on valid button input...        
         console = input()[0]
         a = console == 'a'
-        x = console =='x'
-        y = console =='y'
+        x = console == 'x'
+        y = console == 'y'
         while not a and not y and not x:
             console = input()[0]
             a = console == 'a'
-            x = console =='x'
-            y = console =='y'
+            x = console == 'x'
+            y = console == 'y'
         # Exit...
         if y:
             break
@@ -318,16 +321,16 @@ def demonstrate() -> None:
             save_str = str(demo_index).zfill(3)
             video_recorder.save(f"{save_str}.mp4")
 
-            rgb_np = np.expand_dims(np.array(rgbs), axis=0) # 1, horizon, h, w, c
-            ja_np = np.expand_dims(np.array(jas), axis=0) # 1, horizon, 7
-            eefpose_np = np.expand_dims(np.array(eef_poses), axis=0) # 1, horizon, 7
-
-            dset.add_traj(rgbs=rgb_np, joint_angles=ja_np, eef_poses=eefpose_np)
+            rgb_np = np.expand_dims(np.array(rgbs), axis=0)  # 1, horizon, h, w, c
+            ja_np = np.expand_dims(np.array(jas), axis=0)  # 1, horizon, 7
+            eefpose_np = np.expand_dims(np.array(eef_poses), axis=0)  # 1, horizon, 7
+            print(rgb_np.shape)
+            dset.add_traj(rgbs=rgb_np, joint_angles=ja_np, eef_poses=eefpose_np)  # TODO add  joint_angles=ja_np bcak in
             demo_index += 1
 
     # And... that's all folks!
     print("[*] Done Demonstrating -- Cleaning Up! Thanks 🤖🚀")
 
+
 if __name__ == "__main__":
     demonstrate()
-    
